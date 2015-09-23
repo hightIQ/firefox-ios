10ab76f2b02387b623455c1edcfdb38c5904dd0d
Richard Newman <rnewman@twinql.com>
2015-09-23 12:20:05 -0700

    Logging in mirror record application.

--- Storage/SQL/SQLiteBookmarks.swift
+++ Storage/SQL/SQLiteBookmarks.swift
@@ -344,6 +344,10 @@ extension SQLiteBookmarks: BookmarkMirrorStorage {
             "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
             for args in values {
+                let guid = args[16]
+                let title = args[9]
+                let parent = args[5]
+                log.debug("Updating record with GUID \(guid), title \(title), parent \(parent).")
                 if let error = conn.executeChange(update, withArgs: args) {
                     log.error("Updating mirror: \(error.description).")
                     err = error
@@ -352,9 +356,11 @@ extension SQLiteBookmarks: BookmarkMirrorStorage {
                 }
 
                 if conn.numberOfRowsModified > 0 {
+                    log.debug("Update succeeded. Not inserting.")
                     continue
                 }
 
+                log.debug("Inserting record with GUID \(guid), title \(title), parent \(parent).")
                 if let error = conn.executeChange(insert, withArgs: args) {
                     log.error("Inserting mirror: \(error.description).")
                     err = error
